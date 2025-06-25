1e9b6bdcf7d79688e04c457fc60ae52ad4fc8376
AhsanBadami <144908275+Ahsn23@users.noreply.github.com>
2025-06-25 16:18:23 +0500

    allowed for All

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const rateLimit = require("express-rate-limit");
-const cors = require("cors"); 
+const cors = require("cors");
 const handleSurvey = require("./src/controller/handleSurvey");
 
 const app = express();
@@ -9,7 +9,9 @@ const port = 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
